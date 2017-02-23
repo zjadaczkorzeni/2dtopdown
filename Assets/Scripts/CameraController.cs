b16e88e59fe5ee4bf514c4db6641c131a14fafac
DESKTOP-F42KILP\Diabli <chudydiabel@gmail.com>
2017-02-23 16:32:34 +0100

    Kamera dodana

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform target;
+    public float speed = 1.5f;
+    // Use this for initialization
+    void Start () {
+        target = GameObject.Find("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 dir = target.position - transform.position;
+        dir.z = 0.0f;
+        transform.position += dir * speed * Time.deltaTime;
+        Vector3 maxPos = new Vector3(7.8f, 0, -10);
+        Vector3 minPos = new Vector3(-0.1f, -4.2f, -10);
+        if (transform.position.x >= maxPos.x)
+            transform.position = new Vector3(maxPos.x, transform.position.y, transform.position.z);
+        if (transform.position.y >= maxPos.y)
+            transform.position = new Vector3(transform.position.x, maxPos.y, transform.position.z);
+        if (transform.position.x <= minPos.x)
+            transform.position = new Vector3(minPos.x, transform.position.y, transform.position.z);
+        if(transform.position.y <= minPos.y)
+            transform.position = new Vector3(transform.position.x, minPos.y, transform.position.z);
+    }
+}
